186f04a32430a951666f43aea0b18a17fa5ca1b6
JarmoAr <148751843+JarmoAr@users.noreply.github.com>
2026-06-01 14:41:38 +0300

    Luottu log_service-testit ja korjattu merkistökoodaus UTF-8 muotoon

--- log_service.py
+++ log_service.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 def virhe_logi(virheviesti, logitiedosto):
     try:
-        with open(logitiedosto, "a") as f:
+        with open(logitiedosto, "a", encoding="utf-8") as f:
             aikaleima = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             f.write(f"{aikaleima} - {virheviesti}\n")
         return True
     except Exception as e:
-        return False
+        return None
