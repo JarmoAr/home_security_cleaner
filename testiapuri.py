90aa6c26f06fa79b2452a8d4d74eadd790bb34c0
JarmoAr <148751843+JarmoAr@users.noreply.github.com>
2026-06-04 10:00:51 +0300

    Mokattu face_recognition ja ultralytics testiapurissa pilviajoa varten

--- testiapuri.py
+++ testiapuri.py
@@ -1,3 +1,29 @@
+import sys
+from types import ModuleType
+
+# 1. Tehdään feikki face_recognition -kirjasto
+feikki_fr = ModuleType("face_recognition")
+feikki_fr.load_image_file = lambda polku: "feikkikuva"
+feikki_fr.face_encodings = lambda kuva: [[0.1, 0.2, 0.3]]
+feikki_fr.compare_faces = lambda lista, enco: [True]
+sys.modules["face_recognition"] = feikki_fr
+
+# 2. Tehdään feikki ultralytics (YOLO) -kirjasto
+feikki_ultra = ModuleType("ultralytics")
+class FeikkiYOLO:
+    def __init__(self, malli): pass
+    def predict(self, source, verbose=False):
+        class FeikkiTulos:
+            class FeikkiBox:
+                def __init__(self):
+                    self.cls = [0]
+                    self.xyxy = [10, 20, 100, 200]
+            def __init__(self):
+                self.boxes = [FeikkiBox()]
+        return [FeikkiTulos()]
+
+feikki_ultra.YOLO = FeikkiYOLO
+sys.modules["ultralytics"] = feikki_ultra
 
 
 def luo_feikki_service(palautettava_data):
--- tests/vision_service_test.robot
+++ tests/vision_service_test.robot
@@ -1,6 +1,7 @@
 *** Settings ***
-Library    ../vision_service.py
 Library    ../testiapuri.py
+Library    ../vision_service.py
+
 
 *** test cases ***
 Testaa ota kuvankaappaukset try except
@@ -10,3 +11,11 @@ Testaa ota kuvankaappaukset try except
     ${tulos}    vision_service.Ota Kuvakaappaukset    ${None}
     # Varmistetaan, että kuvankaappaukset on tallennettu oikeaan paikkaan.
     Should Be Empty    ${tulos}
+
+Testaa onko tuttu henkilö try except
+    # Tässä testissä tarkistetaan, että onko tuttu henkilö -funktion 
+    # try except toimii odotetusti.
+    
+    ${tulos}    vision_service.Onko Tuttu Henkilo    ${None}
+    # Varmistetaan, että funktio palauttaa odotetun tuloksen.
+    Should Be Equal    ${tulos}    ${None}
